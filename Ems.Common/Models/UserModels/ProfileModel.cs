192fd0cd46034753eaf4b890137b684aaceb37b5
Joo <saydullayev_jahongir@mail.ru>
2025-05-03 16:32:39 +0500

    novie izmenenie vsyo bilo uspeshno

---
+++ Ems.Common/Models/UserModels/ProfileModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Ems.Common.Models.UserModels;
+public class ProfileModel
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? PhoneNumber { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public int GenderId { get; set; }
+}
